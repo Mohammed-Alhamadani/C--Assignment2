dc9a363fa0a4f0c8e8242db4ec4aa5b502039940
Mohammed-Alhamadani <mbmw.basil7@gmail.com>
2024-06-21 23:51:51 -0400

    Add project files.

---
+++ Controllers/J1ProblemController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+using System.Xml.Schema;
+using static System.Net.WebRequestMethods;
+
+namespace Assignment2.Controllers
+{
+    // <summary>
+    // its a Program to Calculate the calories inside the meal that been orderd.
+    // <summary>
+
+    public class J1ProblemController : ApiController
+    {
+
+        [HttpGet]
+        [Route("api/J1Problem/menu/{burger}/{drink}/{side}/{dessert}")]
+
+        // <param> params names are
+        // {burger} - Integer representing the index burger choice.
+        // {drink} - Integer representing the index drink choice.
+        // {side} - Integer representing the index side choice.
+        // {dessert} - Integer representing the index dessert choice.
+        // </param>
+        // <example>
+        // GET : api/J1Problem/menu/4/4/4/4
+        // http://localhost:58148/api/J1Problem/menu/4/4/4/4
+        // <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">Your Total Calories are 0</string>
+        // GET : api/J1Problem/menu/1/2/3/4
+        // http://localhost:58148/api/J1Problem/menu/1/2/3/4
+        // <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">Your Total Calories are 691</string>
+        // </example>
+        public string Menu(int burger, int drink, int side, int dessert)
+
+        {
+            // Here declaration of the Total Calories.
+            int totalCalories = 0;
+
+
+            //Here checking the input and give it the value of the calories for the Burger.
+
+            if (burger == 1)
+            {
+                totalCalories += 461;
+
+            }
+            else if (burger == 2)
+            {
+                totalCalories += 431;
+            }
+            else if (burger == 3)
+            {
+                totalCalories += 420;
+            }
+            else if (burger == 4)
+            {
+                totalCalories += 0;
+            }
+            //Here checking the input and give it the value of the calories for the Drink.
+
+            if (drink == 1)
+
+            { totalCalories += 130; }
+
+            else if (drink == 2)
+
+            { totalCalories += 160; }
+            else if (drink == 3)
+
+            { totalCalories += 118; }
+
+            else if (drink == 4)
+
+            { totalCalories += 0; }
+
+            //Here checking the input and give it the value of the calories for the Side.
+
+            if (side == 1)
+
+            { totalCalories += 100; }
+
+            else if (side == 2)
+            { totalCalories += 57; }
+
+            else if (side == 3)
+            { totalCalories += 70; }
+
+            else if(side == 4)
+            { totalCalories += 0; }
+
+
+            ////Here checking the input and give it the value of the calories for the Dessert.
+            
+            if (dessert == 1)
+            { totalCalories += 167; }
+
+            else if (dessert == 2)
+            { totalCalories += 266; }
+
+            else if (dessert == 3)
+            { totalCalories += 75; }
+
+            else if (dessert == 4)
+            { totalCalories += 0; }
+
+            // <returns>
+            // how many Calories in Total with the all order.
+            // http://localhost:58148/api/J1Problem/menu/4/4/4/4
+            // <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">Your Total Calories are 0</string>
+            // http://localhost:58148/api/J1Problem/menu/1/2/3/4
+            // <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">Your Total Calories are 691</string>
+            // </returns>
+
+            string Output = "Your Total Calories are "+ totalCalories;
+            return Output;
+
+
+        }
+    }
+    
+
+}
+
+
+
+
---
+++ Controllers/J2ProblemController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace Assignment2.Controllers
+{
+    // <summary>
+    // its a Program to Calculate how many numbers in the Dice that the sum of them equal 10.
+    // <summary>
+    public class J2ProblemController : ApiController
+    {
+        //<Param> has two params as input integer</Param>
+
+        [HttpGet]
+        [Route("api/J2Problem/Dice/{m}/{n}")]
+        public String Dice(int m, int n)
+        {
+
+            //<example>
+            //GET : api/J2Problem/Dice/6/8
+            //http://localhost:58148/api/J2Problem/Dice/6/8
+            //<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">there are 5 total ways to get the sum 10</string>
+            //GET : api/J2Problem/Dice/12/4
+            //http://localhost:58148/api/J2Problem/Dice/12/4
+            //<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">there are 4 total ways to get the sum 10</string>
+            //GET : api/J2Problem/Dice/3/3
+            //http://localhost:58148/api/J2Problem/Dice/3/3
+            //<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">there are 0 total ways to get the sum 10</string>
+            //GET : api/J2Problem/Dice/5/5
+            //http://localhost:58148/api/J2Problem/Dice/5/5
+            //<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">there are 1 total ways to get the sum 10</string>
+            //</example>
+
+            //Here Checking the inputs if it Negative or equal 0.
+
+            if (m<=0 || n<=0)
+            {
+                string Output = "Please Enter Positive Number!";
+                return Output;
+            }
+
+            //here Decalration for the result 
+
+            int result = 0;
+
+            //here doing iteration for inputs M & N to checl all the numbers which two numbers will return 10.
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    //if this condition true will count the ways
+                    if (i + j == 10)
+                    {
+                        result++;
+                    }
+                }
+            }
+            string response = "there are " + result + " total ways to get the sum 10";
+            return response;
+        }
+    }
+}
